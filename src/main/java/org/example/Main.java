fef1a4f5b1196fce9cf4e694fded73af852cb850
Arshdeep Kaur <arshdeepk61@gmail.com>
2025-11-16 12:01:04 -0500

    Ignore target folder

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,14 @@
 package org.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.audit.AuditEventsEndpointAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
-@SpringBootApplication(scanBasePackages = {"org"})
-@EnableAspectJAutoProxy
 
+@SpringBootApplication
+@EnableAspectJAutoProxy
 public class Main {
     
     public static void main(String[] args) {
